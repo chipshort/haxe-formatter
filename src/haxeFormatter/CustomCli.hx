5165df896be162ef6fb7989e618407958787ce52
Christoph Otter <christophotter@gmail.com>
2017-10-13 13:58:27 +0200

    Add real cli interface

---
+++ src/haxeFormatter/CustomCli.hx
@@ -0,0 +1,89 @@
+package haxeFormatter;
+
+import haxeFormatter.util.CliMacro;
+import sys.FileSystem;
+import sys.io.File;
+using StringTools;
+
+/**
+ * ...
+ * @author Christoph Otter
+ */
+class CustomCli 
+{
+	static function main() {
+        new CustomCli();
+    }
+
+	var config:Config;
+
+    function new() {
+		config = {
+			imports: {},
+			padding: {
+				colon: {},
+				binaryOperator: {},
+				insideBrackets: {},
+				comma: {},
+				questionMark: {},
+				//TODO: finish
+			},
+			indent: {},
+			braces: {newlineBeforeOpening: {}}
+		}
+		
+		//var map : Map<Array<String>, Dynamic> = CliMacro.getArgs();
+		
+		//hxargs.Args.generate(CliMacro.getArgs());
+        var args = Sys.args();
+        var paths = [];
+		
+        var argHandler = CliMacro.getArgs();
+		
+        argHandler.parse(args);
+        if (args.length == 0) {
+            Sys.println("Haxe Formatter");
+            Sys.println(argHandler.getDoc());
+            Sys.exit(0);
+        }
+		
+		try {
+			var code = readAll(Sys.stdin()).toString();
+			formatCode(code);
+		} catch (e : Any) {
+			trace(e);
+		}
+		
+    }
+	
+	inline function readAll(input:haxe.io.Input) {
+		var bufsize = (1 << 14); // 16 Ko
+
+		var buf = haxe.io.Bytes.alloc(bufsize);
+		var total = new haxe.io.BytesBuffer();
+		try {
+			while( true ) {
+				var len = input.readBytes(buf, 0, bufsize);
+				if( len == 0 )
+					throw haxe.io.Eof;
+				total.addBytes(buf,0,len);
+			}
+		} catch( e : Any ) { }
+		return total.getBytes();
+	}
+
+	function formatCode(code:String) {
+		var formatted = Formatter.formatSource(code, config);
+        switch (formatted) {
+            case Success(data):
+                Sys.print(data);
+            case Failure(reason):
+                error(reason);
+        }
+	}
+
+    function error(reason:String) {
+        Sys.stderr().writeString('Could not format code: $reason\n');
+    }
+	
+}
---
+++ src/haxeFormatter/util/CliMacro.hx
@@ -0,0 +1,120 @@
+package haxeFormatter.util;
+import haxe.macro.Context;
+import haxe.macro.Expr;
+import haxe.macro.Type;
+import haxe.macro.TypeTools;
+import haxeFormatter.util.CliMacro.FieldTree;
+
+using StringTools;
+using Lambda;
+
+/**
+ * ...
+ * @author Christoph Otter
+ */
+class CliMacro {
+	
+	macro public static function getArgs() : Expr {
+		var configType = Context.follow(Context.getType("haxeFormatter.Config"));
+		
+		configType.match(TAnonymous(fields));
+		switch (configType) {
+			case TAnonymous(type):
+				var access = "config.";
+				
+				var t = type.get();
+				var tree = new FieldTree(null, "config", null);
+				
+				for (field in t.fields) {
+					tree.addSubField(field);
+				}
+				
+				var argExprs = new Array<Expr>();
+				
+				var leaves = tree.getAllSubFields().filter(function (f) return f.isLeaf());
+				for (l in leaves) {
+					var path = l.toPath();
+					var type = l.type;
+					
+					var typePath = TypeTools.toComplexType(type);
+					trace(type);
+					trace(typePath);
+					
+					var arg = "--" + path.join("-").substr("config".length);
+					var expr = macro @doc($v{l.doc == null ? "" : l.doc}) [$v{arg}] => function(arg) ${Context.parse(path.join("."), Context.currentPos())} = arg;
+					argExprs.push(expr);
+					trace(expr);
+				}
+				
+				return haxeFormatter.util.ArgsHelper.generate(macro $a{argExprs});
+			default:
+				throw "haxeFormatter.Config should be an anonymous structure";
+		}
+		
+		trace(configType);
+		
+		return macro {};
+	}	
+}
+
+#if macro
+
+class FieldTree {
+	
+	public var subFields = new Array<FieldTree>();
+	public var parent : FieldTree;
+	public var name : String;
+	public var type : Type;
+	public var doc : String;
+	
+	public function new(p : FieldTree, n : String, t : Type) {
+		parent = p;
+		name = n;
+		type = t;
+	}
+	
+	public function addSubField(field : ClassField) {
+		var fieldTree = new FieldTree(this, field.name, field.type);
+		fieldTree.doc = field.doc;
+		subFields.push(fieldTree);
+		
+		var fieldType = Context.follow(field.type);
+		
+		switch (fieldType) {
+			case TAnonymous(type):
+				for (field in type.get().fields)
+					fieldTree.addSubField(field); //add all subFields recursively
+			default:
+				//fieldType has no subfields
+		}
+	}
+	
+	public function toPath() : Array<String> {
+		if (parent == null)
+			return [name];
+		
+		var path = parent.toPath();
+		path.push(name);
+		return path;
+	}
+	
+	/**
+	 * Gets subFields and subsubFields, ...
+	 * @return
+	 */
+	public function getAllSubFields() : Array<FieldTree> {
+		var array = subFields;
+		
+		for (sub in subFields) {
+			array = array.concat(sub.getAllSubFields());
+		}
+		
+		return array;
+	}
+	
+	public function isLeaf() : Bool {
+		return subFields.length == 0;
+	}
+}
+
+#end
